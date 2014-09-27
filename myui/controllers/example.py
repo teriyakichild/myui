2015bfe04823bcc695363dc747e6c323eb0bc575
Tony Rogers <tony.rogers@rackspace.com>
2014-09-26 23:42:40 -0500

    adding an example

---
+++ myui/controllers/example.py
@@ -0,0 +1,12 @@
+from myui.controllers import BaseHandler
+import tornado.web
+
+
+class params:
+    route='/example'
+    pass
+
+class Handler(BaseHandler):
+    @tornado.web.removeslash
+    def get(self):
+        self.render('example.j2')
